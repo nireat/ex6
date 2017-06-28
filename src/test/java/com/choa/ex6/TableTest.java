6464d18ba686941d28f19085338377c720c8cb17
user2-PC\user2 <bbokbbok91@gmail.com>
2017-06-28 18:55:35 +0900

    0628

--- src/test/java/com/choa/ex6/TableTest.java
+++ src/test/java/com/choa/ex6/TableTest.java
@@ -18,10 +18,10 @@ public class TableTest extends MyAbstractTestUnit{
 	@Test
 	public void test() throws Exception{
 		TableADTO tableADTO = new TableADTO();
-		tableADTO.setNum(2);
-		tableADTO.setTitle("t2");
-		tableADTO.setWriter("t2");
-		int result = tableService.insertTable(tableADTO, tableADTO.getNum());
+		tableADTO.setNum(4);
+		tableADTO.setTitle("t3");
+		tableADTO.setWriter("t3");
+		int result = tableService.insertTable(tableADTO, 1);
 		assertNotEquals(0, result);
 	}
 }
